2ba072ad927033ecfced1ecbb43fa641bf8a563f
nandinidas07 <nandinidas107@gmail.com>
2025-07-24 22:15:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -20,4 +20,26 @@ describe("Transforming Data with Promises", () => {
     cy.wait(1000);
     cy.get("#output").should("contain", "Final Result: 13.5");
   });
+
+  it("should perform all transformations correctly for input 10", () => {
+    cy.visit(baseUrl + "/main.html");
+
+    cy.get("#ip").type("10");
+    cy.get("#btn").click();
+
+    cy.wait(2000);
+    cy.get("#output").should("contain", "Result: 10");
+
+    cy.wait(1000);
+    cy.get("#output").should("contain", "Result: 20");
+
+    cy.wait(1000);
+    cy.get("#output").should("contain", "Result: 17");
+
+    cy.wait(1000);
+    cy.get("#output").should("contain", "Result: 8.5");
+
+    cy.wait(1000);
+    cy.get("#output").should("contain", "Final Result: 18.5");
+  });
 });
